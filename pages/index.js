b223647865542116b82a6e8add6aafa0488c6138
Hao Chen <hche8093@uni.sydney.edu.au>
2023-06-28 00:55:21 +1000

    Added auth

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
-	return <div>Hello World</div>
+	return (
+		<div>
+			<h1>this is the homepage</h1>
+			<div>
+				<Link href='/api/auth/login'>Login</Link>
+			</div>
+		</div>
+	)
 }
