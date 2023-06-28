2a7d01c03201d7aae79554e797c07bc3641c0798
Hao Chen <hche8093@uni.sydney.edu.au>
2023-06-28 23:48:47 +1000

    Added sidebar and styling

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,14 +1,17 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { AppLayout } from '../../components/AppLayout'
 
-export default function New({ test }) {
-	return <div>{test}</div>
+export default function NewPost({}) {
+	return <div>new post page</div>
+}
+
+NewPost.getLayout = function getLayout(page, pageProps) {
+	return <AppLayout {...pageProps}>{page}</AppLayout>
 }
 
 // props for the component in SSR
 export const getServerSideProps = withPageAuthRequired(() => {
 	return {
-		props: {
-			test: 'this is a test',
-		},
+		props: {},
 	}
 })
