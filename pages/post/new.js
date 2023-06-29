cec5d7a2e1f0c300625695e2a7630d5d565080ae
Hao Chen <hche8093@uni.sydney.edu.au>
2023-06-29 13:02:21 +1000

    Add openai endpoints

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,8 +1,32 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { AppLayout } from '../../components/AppLayout'
+import { useState } from 'react'
 
 export default function NewPost({}) {
-	return <div>new post page</div>
+	const [postContent, setPostContent] = useState('')
+
+	const handleClick = async () => {
+		const response = await fetch('/api/generatePost', {
+			method: 'POST',
+		})
+
+		const json = await response.json()
+		console.log(json.post.postContent)
+		setPostContent(json.post.postContent)
+	}
+
+	return (
+		<div>
+			<h1>New Post</h1>
+			<button className='btn' onClick={handleClick}>
+				Generate
+			</button>
+			<div
+				className='max-w-screen-sm p-10'
+				dangerouslySetInnerHTML={{ __html: postContent }}
+			></div>
+		</div>
+	)
 }
 
 NewPost.getLayout = function getLayout(page, pageProps) {
